7a1e6ebcac8795c6bf26dc680a57a711e4a57312
Leastorga <leticia.trancozo@gmail.com>
2023-12-19 13:13:30 -0300

    Entendendo a arquitetura MVC

---
+++ SaborExpress/Controllers/TesteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaborExpress.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+
+    }
+}
