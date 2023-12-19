85f7ae6dd88294cec114d66db65ae35a42b2662b
Leastorga <leticia.trancozo@gmail.com>
2023-12-19 11:26:32 -0300

    Add project files.

---
+++ SaborExpress/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaborExpress.Models;
+using System.Diagnostics;
+
+namespace SaborExpress.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index() // Está retornando o view ao acessar a página, isso se deve a interface IActionResult
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
