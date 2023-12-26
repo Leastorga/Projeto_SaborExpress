5108be2f4d681e6824b5c5266627842b78da50dc
Leastorga <leticia.trancozo@gmail.com>
2023-12-26 10:05:26 -0300

    populando a tabela categoria

---
+++ SaborExpress/Migrations/20231226130323_PopularCategories.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaborExpress.Migrations
+{
+    public partial class PopularCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories(CategoryName, Description)" + "VALUES('Normal', 'Snack made with normal ingredients')");
+            migrationBuilder.Sql("INSERT INTO Categories(CategoryName, Description)" + "VALUES('Natural', 'Snack made with whole and natural ingredients')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories");
+        }
+    }
+}
