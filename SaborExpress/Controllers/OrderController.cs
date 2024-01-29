06e87ce80ce7baa5f97ba8024ffa079aa3e1b372
Leastorga <leticia.trancozo@gmail.com>
2024-01-29 10:38:02 -0300

    Criando o método Action Checkout

--- SaborExpress/Controllers/OrderController.cs
+++ SaborExpress/Controllers/OrderController.cs
@@ -15,15 +15,55 @@ namespace SaborExpress.Controllers
             _shoppingCart = shoppingCart;
         }
 
-        public ActionResult Checkout()
+        [HttpGet]
+        public IActionResult Checkout()
         {
             return View();
         }
-
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
-            return View();
+            int totalItemsOrder = 0;
+            decimal totalOrderPrice = 0.0m;
+
+            // Obtem os itens do carrinho de compra do cliente
+            List<CartPurchaseItem> items = _shoppingCart.GetCartPurchaseItems();
+            _shoppingCart.CartPurchaseItems = items;
+
+            //Verifica se existem itens de pedido
+            if (_shoppingCart.CartPurchaseItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, how about adding a snack...");
+            }
+            // Calcular o total de items e o total do pedido
+            foreach (var item in items)
+            {
+                totalItemsOrder += item.Quantity;
+                totalOrderPrice += (item.Snack.Price * item.Quantity);
+            }
+
+            //Atribui os valores obtidos ao pedido
+            order.TotalItemsOrder = totalItemsOrder;
+            order.TotalOrder = totalOrderPrice;
+
+            //Valida os dados pedido
+            if (ModelState.IsValid)
+            {
+                //Cria o pedido e os detalhes
+                _orderRepository.CreateOrder(order);
+
+                //Define mensagens ao cliente
+                ViewBag.CheckoutCompleteMessage = "Thank you for your order :)";
+                ViewBag.TotalOrder = _shoppingCart.GetTotalShoppingCart();
+
+                //Limpa o carrinho do cliente
+                _shoppingCart.CleanCart();
+
+                //Exibe a view com dados do cliente e do pedido
+
+                return View("~/Views/Order/CheckoutComplete.cshtml", order);
+            }
+            return View(order);
         }
     }
 }
