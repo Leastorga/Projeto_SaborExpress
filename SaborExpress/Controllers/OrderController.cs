a807900dd971949bf1dea4bb150d30fc8fe94641
Leastorga <leticia.trancozo@gmail.com>
2024-01-29 10:00:28 -0300

    Criando o controlador PedidoController e criando a View Checkout

---
+++ SaborExpress/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaborExpress.Models;
+using SaborExpress.Repositories.Interfaces;
+
+namespace SaborExpress.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            return View();
+        }
+    }
+}
