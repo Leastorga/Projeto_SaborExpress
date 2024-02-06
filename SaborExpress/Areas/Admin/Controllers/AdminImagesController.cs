71aefd128d61e67b6e9364f921a1fdede6e2f266
Leastorga <leticia.trancozo@gmail.com>
2024-02-06 10:35:44 -0300

    Enviando arquivos

--- SaborExpress/Areas/Admin/Controllers/AdminImagesController.cs
+++ SaborExpress/Areas/Admin/Controllers/AdminImagesController.cs
@@ -22,5 +22,40 @@ namespace SaborExpress.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Erro"] = "Error: Unselected file(s)";
+                return View(ViewData);
+            }
+
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Error: Number of files exceeded the limit";
+                return View(ViewData);
+            }
+
+            long size = files.Sum(f => f.Length);
+            var filePathsName = new List<string>();
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, _myConfig.NameFolderImagesProducts);
+            foreach (var formFile in files)
+            {
+                if (formFile.FileName.Contains(".jpg") || formFile.FileName.Contains(".gif") || formFile.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
+                    filePathsName.Add(fileNameWithPath);
+                    
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            ViewData["Result"] = $"{files.Count} Files have been uploaded to the server, " + $"with a total size of : {size} bytes";
+            ViewBag.Files = filePathsName;
+            return View(ViewData);
+        }
     }
 }
