d89ecd8f1555fd99ba69fd70cf21e9226c29d59c
Leastorga <leticia.trancozo@gmail.com>
2024-02-06 11:27:09 -0300

    Criando o modelo de dominío gráfico

---
+++ SaborExpress/Models/GraficSnack.cs
@@ -0,0 +1,9 @@
+﻿namespace SaborExpress.Models
+{
+    public class GraficSnack
+    {
+        public string SnackName { get; set; }
+        public int SnacksQuantity { get; set; }
+        public decimal SnacksTotalValue { get; set; }
+    }
+}
